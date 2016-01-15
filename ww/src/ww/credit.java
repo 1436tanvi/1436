22015b61d5273d93c0f5dcd1de8f0eeb4de4291f
Tanvi <Tanvi@HP15R>
2016-01-15 14:55:41 +0530

    nn

---
+++ ww/src/ww/credit.java
@@ -0,0 +1,31 @@
+package ww;
+
+public class credit
+{
+	private int Items;
+		int choice;
+		
+		
+		
+		void credidcard() {
+			
+			if (choice==1){
+			
+			}}
+			
+
+		void debitcardn() {
+			
+			if (choice==2){
+			
+			}
+			}
+
+			
+		
+		
+		
+		
+		
+
+}
