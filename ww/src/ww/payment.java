b023d6bfb4e4f09f82bca5b6a534e003a0672862
Tanvi <Tanvi@HP15R>
2016-01-15 14:54:06 +0530

    nn

---
+++ ww/src/ww/payment.java
@@ -0,0 +1,25 @@
+package ww;
+
+
+
+/*elevatorclient*/
+import java.io.IOException;
+		
+public class payment {
+public static void main(String args[]) throws IOException {
+int choice = 0;
+credit  E = new credit();
+pa P = new pa();
+
+
+// User now presses button for elevator
+if (choice == 1) {
+System.out.println("payment is by credit card:");
+} else if (choice==2) {
+System.out.println("payment is by debit card:");
+
+
+}
+}}
+
+
