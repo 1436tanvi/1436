7e631b53547e69ed87d125c0bd613d1e14a97263
Tanvi <Tanvi@HP15R>
2016-01-15 14:56:38 +0530

    nn

---
+++ ww/src/ww/pa.java
@@ -0,0 +1,48 @@
+package ww;
+
+public class pa {
+
+
+
+
+		private int pannelFloor;
+		
+		pa (){
+		pannelFloor = ((int) (Math.random() * 5 + 1));
+		}
+		
+		
+		int getUserCurrentFloor() {
+		return pannelFloor;
+		}
+
+		void getUserCurrentFloor(int x) {
+		pannelFloor = x;
+		}
+		}
+
+		 
+
+		 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
